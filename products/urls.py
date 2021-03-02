872f5a6685ac8162249888c7a11d3671ef427531
shotttik <shota.akhlouri@gmail.com>
2021-03-02 20:01:35 +0400

    added homepage

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from products.views import create
+from products.views import create, detail, upvote
 
 urlpatterns = [
-    path('create', create, name='create')
+    path('create', create, name='create'),
+    path('detail/<int:product_id>', detail, name='detail'),
+    path('detail/<int:product_id>/upvote', upvote, name='upvote')
 ]
--- products/views.py
+++ products/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 
+
 def homepage(request):
-    return render(request, 'pages/homepage.html', )
+    products = Product.objects.all()
+    return render(request, 'pages/homepage.html', {'products': products})
 
 
-@login_required()
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == "POST":
         product = Product()
@@ -17,5 +19,19 @@ def create(request):
         product.image = request.FILES['image']
         product.hunter = request.user
         product.save()
-        return redirect('homepage')
+        return redirect('/products/detail/' + str(product.id))
     return render(request, 'pages/products/create.html', )
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'pages/products/detail.html', {'product': product})
+
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()   # ცვლის იმ პროდუქტის ინფოს ბაზაში რომელსაც upvote-ს ვაძლევთ
+        return redirect('/products/detail/' + str(product.id))
